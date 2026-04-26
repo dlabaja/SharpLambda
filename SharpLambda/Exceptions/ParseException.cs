1f889b54da3a4cc2c225b14cb2fc14afda978e6b
dlabaja <dlabaja.jan@seznam.cz>
2026-04-26 23:02:41 +0200

    parse exception message

--- SharpLambda/Exceptions/ParseException.cs
+++ SharpLambda/Exceptions/ParseException.cs
@@ -1,3 +1,3 @@
 namespace SharpLambda.Exceptions;
 
-public class ParseException(string expr) : Exception($"Cannot parse the sexpression {expr}");
+public class ParseException(string expr) : Exception($"Cannot parse the expression {expr}");
