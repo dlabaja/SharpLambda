8dfecd8884b98344b21e9b712078f6e0cdd47e78
dlabaja <dlabaja.jan@seznam.cz>
2026-05-02 15:40:54 +0200

    testy define

--- SharpLambda/Program.cs
+++ SharpLambda/Program.cs
@@ -1,8 +1,37 @@
-﻿using SharpLambda;
-using SharpLambda.DataTypes;
+﻿using SharpLambda.DataTypes;
 
-//var context = new Context();
-//var expr = Interpreter.Eval("((lambda (a b) a) 1 2 d)", context);
-//Console.WriteLine(expr);
+namespace SharpLambda;
 
-new Test().TestInterpreter();
+internal static class Program
+{
+    private static readonly Context _context = new Context();
+    
+    public static void Main(string[] args)
+    {
+        Eval("(define true (λ (a b) a))");
+        Eval("(define false (λ (a b) b))");
+        Eval("(define if (λ (p c a) (p c a)))");
+        Eval("(define cons (λ (x y z) (z x y)))");
+        Eval("(define car (λ (p) (p true)))");
+        Eval("(define cdr (λ (p) (p false)))");
+        Eval("(define succ (λ (n) (cons false n)))");
+        Eval("(define pred (λ (n) (cons false n)))");
+        Eval("(define zero (λ (x) x))");
+        Eval("(define one (λ (z) (z false zero)))");
+        Eval("(define two (λ (z) (z false one)))");
+        Eval("(define three (λ (z) (z false two)))");
+        Eval("(define four (λ (z) (z false three)))");
+        Eval("(define zero? car)");
+        Eval("(define i+% (λ (f a b) (if (zero? b) a (f f (succ a) (pred b)))))");
+        Eval("(define i+ (λ (a b) (i+% i+% a b)))");
+        Eval("(i+ one one)");
+    }
+
+    private static void Eval(string expr)
+    {
+        Console.WriteLine(Interpreter.Eval(expr, _context));
+    }
+}
+
+// testování -> pusťte v Debug modu tuhle metodu, pokud se debugger na něčem zasekne tak se něco vyhodnocuje špatně
+// new Test().TestInterpreter();
